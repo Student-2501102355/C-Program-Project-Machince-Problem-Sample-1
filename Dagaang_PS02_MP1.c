cd3a9fee117b1ba1e119724dcc126afbd71623b5
A Student-Numbered_2501102355 <2501102355@student.buksu.edu.ph>
2025-11-28 10:32:23 +0800

    Add files via upload

---
+++ Dagaang_PS02_MP1.c
@@ -0,0 +1,208 @@
+#include <stdio.h>
+
+int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    int main(){
+    int almera, lawrence, steeve, carlos, dagpong;
+
+    printf("Enter almera: ");
+    scanf("%d", &almera);
+
+    printf("Enter lawrence: ");
+    scanf("%d", &lawrence);
+
+    steeve = lawrence * (lawrence / almera) + almera * almera;
+    carlos = steeve * almera + lawrence + (almera / steeve) + steeve;
+    dagpong = carlos * (steeve + almera) * lawrence / steeve + carlos + (steeve * steeve);
+
+    printf("\nSteve is gwapo: %d\n", steeve);
+    printf("\nCarlos is pogi: %d\n", carlos);
+    printf("\nDagpong is soo handsome: %d\n", dagpong);
+
+    return 0;
+}
